e62da66e02cef2f11e5f2786338bd9f4d7c0737e
Proloy Das <proloy@umd.edu>
2018-05-11 18:50:49 -0400

    Class definition

--- champagne.py
+++ champagne.py
@@ -1,184 +1,60 @@
-# -*- coding: utf-8 -*-
 """
-Module implementing Champagne:
-
-An fast implementation of Champagne algorithm.
-
-Reference--
-Wipf, David P., et al. "Robust Bayesian estimation of the location, orientation, and time
-course of multiple correlated neural sources using MEG." NeuroImage 49.1 (2010): 641-655.
-
-Created on Tues Mar 24 2018
-
-@author: Proloy Das
-
-@licence = apache 2.0
+Module implementing Champagne
 """
+
+__author__ = 'Proloy DAS'
+__licence__ = 'apache 2.0'
+
 import numpy as np
 from scipy import linalg
+from eelbrain import *
 from math import sqrt
 import tqdm
 import time
 
-
-def champagne(L, B, noise_cov, T, idata_cov, dc, max_Iter=30, verbose=True ):
+def _myinv(x):
     """
 
-    :param L: lead-field matrix
-            (K, N) 2D array
-            uses either fixed or free orientation lead-field vectors.
-    :param B: meg data
-            (K, T) 2D array
-    :param noise_cov: noise co-variance
-            (K, K) 2D array
-    :param T: previous estimates of covariance of source activities
-            list [#sources (dc, dc) 2D arrays]
-    :param idata_cov: previous estimates of inverse data covariances
-            (K, K) 2D array
-    :param dc: orientation
-            scalar integer
-            1 = fixed, 3 = free
-    :param max_Iter: self-explained
-            scalar integer
-            Maximum number of iterations, usually set at 30.
+    Computes inverse
 
-    :return: dict of relevant variables:
-            "Tau": list [#sources (dc, dc) 2D arrays]
-            "inverse_kernel": (N, M) 2D array
-            "isigma_b": (M, M) 2D array
-            "objective": 1D array
+    parameters
+    ----------
+    x: ndarray
+    array of shape (dc, dc)
 
-    Examples:
-
-    # for free orientation
-    >>> dc = 3
-    # pre-whiten data
-    >>> wf = linalg.cholesky(noise_cov, lower=True)
-    >>> y = linalg.solve(wf, y)
-    >>> L = linalg.solve(wf, L)
-    >>> noise_cov = np.eye(L.shape[0])
-    # choose initial guess for Tau (same as mne)
-    >>> gamma = L.shape[0]/np.trace(np.dot(y,y.T))
-    >>> num_sources = L.shape[1] / dc
-    >>> Tau = [gamma * np.eye (dc, dtype='float') for _ in range (num_sources)]
-    # Construct inverse data covariances corresponding to that Tau
-    >>> idata_cov = linalg.solve(noise_cov + np.dot(L, np.dot(linalg.block_diag(Tau), L.T)), np.eye(L.shape[0], dtype='float'))
-    >>> output = champagne(L, y, inoise_cov, Tau, idata_cov, dc, max_Iter=50)
-    # Construct source-estimate
-    >>> source_estimate = np.dot (output["inverse_kernel"], np.dot (output["isigma_b"], y))
-
+    returns
+    -------
+    ndarray
+    array of shape (dc, dc)
     """
-    # we replace B with a matrix Bhat K * rank(B) (=K)
-    Cb = np.dot(B, B.T) / B.shape[1]
-    Bhat = linalg.cholesky(Cb, lower=True)
-
-    # Change this to previous to see any change!
-    # [E, V] = linalg.eig(Cb)
-    # Bhat = np.real( np.dot(V, np.dot(np.diag(np.sqrt(E)), np.matrix(V).H)) )
-
-    num_source = (L.shape[1])/dc
-
-    isigma_b = idata_cov
-    inoise_cov = linalg.inv (noise_cov)
-
-    # store values to return
-    obj = np.empty(0)
-    new_obj = np.trace (np.dot (isigma_b, Cb)) - 2 * np.sum (np.log (np.diag (linalg.cholesky (isigma_b))))
-    obj = np.append (obj, new_obj)
-
-    T = list(T)
-
-    # Champagne iterations
-    for _ in tqdm.tqdm(xrange(max_Iter)):
-        time.sleep(0.001)
-        # pre-compute some useful matrices
-        Lhat = np.dot(isigma_b, L)
-
-        if dc == 1:
-            # compute isigma_b for the next iteration
-            new_isigma_b = inoise_cov
-
-            for i in xrange (num_source):
-
-                # update Xi
-                x = np.dot (T[i], np.dot (Bhat.T, Lhat[:, i * dc:(i + 1) * dc]).T)
-
-                # update Zi
-                z = np.dot (L[:, i * dc:(i + 1) * dc].T, Lhat[:, i * dc:(i + 1) * dc])
-
-                # update Ti
-                T[i] = sqrt (np.dot (x, x.T)) / np.real (sqrt (z))
-
-                # update new_isigma_b
-                temp1 = np.dot (new_isigma_b, L[:, i * dc:(i + 1) * dc])
-                new_isigma_b = new_isigma_b - np.dot (temp1, temp1.T) * T[i] / \
-                                   (1 + T[i] * np.dot (L[:, i * dc:(i + 1) * dc].T, temp1))
-
-
-            # Update isigma_b
-            isigma_b = np.copy (new_isigma_b)
-
-        else:
-            # compute isigma_b for the next iteration
-            new_isigma_b = noise_cov
-
-            for i in xrange (num_source):
-
-                # update Xi
-                x = np.dot (T[i], np.dot (Bhat.T, Lhat[:, i * dc:(i + 1) * dc]).T)
-
-                # update Zi
-                z = np.dot (L[:, i * dc:(i + 1) * dc].T, Lhat[:, i * dc:(i + 1) * dc])
-
-                # update Ti
-                T[i] = solve_for_Tau_n(z, x)
-
-                # update new_isigma_b
-                new_isigma_b = new_isigma_b + np.dot (L[:, i * dc:(i + 1) * dc],
-                                                          np.dot (T[i], L[:, i * dc:(i + 1) * dc].T))
-
-            # update isigma_b
-            isigma_b = linalg.inv (new_isigma_b)
-
-        # Calculate fval
-        new_obj = np.trace(np.dot (isigma_b, Cb)) + 2 * np.sum(np.log(np.diag(linalg.cholesky (new_isigma_b))))
-        obj = np.append (obj, new_obj)
-
-    # Return all relevant variables
-    # prepare the inverse kernel
-    inverse_kernel = np.zeros(L.T.shape)
-    for i in xrange(num_source):
-        inverse_kernel[i * dc:(i + 1) * dc, :] = np.dot(T[i], L[:, i * dc:(i + 1) * dc].T)
-
-    out = {"Tau": T,
-            "inverse_kernel": inverse_kernel,
-           "isigma_b": isigma_b,
-           "objective": obj}
-
-    return out
-
-
-def myinv(x):
     x = np.real(np.array(x))
     y = np.zeros(x.shape)
     y[x>0] = 1/x[x > 0]
     return y
 
 
-def solve_for_Tau_n(z, x):
+def _compute_gamma_i(z, x):
     """
-    Computes Tau_i = Z**(-1/2) * ( Z**(1/2) X X' Z**(1/2)) ** (1/2) * Z**(-1/2)
+
+    Computes Gamma_i = Z**(-1/2) * ( Z**(1/2) X X' Z**(1/2)) ** (1/2) * Z**(-1/2)
                    = V(E)**(-1/2)V' * ( V ((E)**(1/2)V' X X' V(E)**(1/2)) V')** (1/2) * V(E)**(-1/2)V'
                    = V(E)**(-1/2)V' * ( V (UDU') V')** (1/2) * V(E)**(-1/2)V'
                    = V (E)**(-1/2) U (D)**(1/2) U' (E)**(-1/2) V'
 
-    :param z: auxiliary variable,  z_i
-            (dc, dc) 2D array
+    parameters
+    ----------
+    z: ndarray
+        array of shape (dc, dc)
+        auxiliary variable,  z_i
 
-    :param x: auxiliary variable, x_i
-            (dc * K) 2D array
+    x: ndarray
+        array of shape (dc, dc)
+        auxiliary variable, x_i
 
-    :return: Tau_i
+    returns
+    -------
+    ndarray
+    array of shape (dc, dc)
 
     """
     [e, v] = linalg.eig(z)
@@ -189,5 +65,262 @@ def solve_for_Tau_n(z, x):
     [d, u] = linalg.eig( (temp * e) * e[:, np.newaxis] )
     d[d < 0] = 0
     d = np.sqrt(d)
-    temp = np.dot(v * myinv(np.real(e)), u)
+    temp = np.dot(v * _myinv(np.real(e)), u)
     return np.array( np.real(np.dot(temp * d, np.matrix(temp).H)) )
+
+
+def _compute_objective(Cb, isigma_b):
+    """
+
+    Compute objective value at a given iteration
+
+    parameters
+    ----------
+    Cb: ndarray
+    array of shape (K, K)
+
+    isigma_b: ndarray
+    array of shape (K, K)
+
+    returns
+    -------
+    float
+
+    """
+    return np.trace(np.dot(isigma_b, Cb)) - 2 * np.sum (np.log(np.diag(linalg.cholesky(isigma_b))))
+
+
+class Champagne:
+    """
+    Champagne algorithm
+
+    Reference--
+    Wipf, David P., et al. "Robust Bayesian estimation of the location, orientation, and time
+    course of multiple correlated neural sources using MEG." NeuroImage 49.1 (2010): 641-655.
+
+    Parameters
+    ----------
+    lead_field: NDVar
+        array of shape (K, N)
+        lead-field matrix.
+        both fixed or free orientation lead-field vectors can be used.
+
+    orientation: 'fixed'|'free'
+        'fixed': orientation-constrained lead-field matrix.
+        'free': free orientation lead-field matrix.
+
+    noise_covariance: ndarray
+        array of shape (K, K)
+        noise covariance matrix
+        use empty-room recordings to generate noise covariance matrix at sensor space.
+
+    n_iter: int, optionnal
+        number of iterations
+        default is 1000
+
+    Attributes
+    ----------
+    Gamma: list
+        list of length N
+        individual source covariance matrices
+
+    inverse_sigma_b: ndarray
+        array of shape (K, K)
+        inverse of data covariance under the model
+
+    objective: list, optional
+        list of objective values at each iteration
+        returned only if verbose=1
+
+    est_data_covariance: ndarray
+        array of shape (K, K)
+        estimated data covariance under the model
+        returned only if verbose=1
+
+    emp_data_covariance: ndarray
+        array of shape (K, K)
+        empirical data covariance
+        returned only if verbose=1
+
+    inverse_kernel: ndarray
+        array of shape (K, K)
+        inverse imaging kernel
+        returned only if return_inverse_kernel=1
+
+
+    """
+    def __init__(self, lead_field, noise_covariance, n_iter=1000):
+        if lead_field.has_dim('space'):
+            self.lead_field = lead_field.get_data (dims=('sensor', 'source', 'space')).astype('float64')
+            self.sources_n = self.lead_field.shape[1]
+            self.lead_field = self.lead_field.reshape(self.lead_field.shape[0], -1)
+            self.orientation = 'free'
+            self.space = lead_field.space
+        else:
+            self.lead_field = lead_field.get_data(dims=('sensor', 'source')).astype('float64')
+            self.sources_n = self.lead_field.shape[1]
+            self.orientation = 'fixed'
+        self.source = lead_field.source
+        self.sensor = lead_field.sensor
+        self.noise_covariance = noise_covariance
+        self.n_iter = n_iter
+
+    def solve(self, meg, verbose=0):
+        """
+
+        Champagne algorithm
+
+        parameters
+        ----------
+        meg: NDVar
+            meg data
+
+        verbose: {0,1}
+            verbosity of the method : 1 will display informations while 0 will display nothing
+            default = 0
+
+        returns
+        -------
+        self
+        """
+
+        y = meg.get_data(('sensor', 'time'))
+        Cb = np.dot(y, y.T) / y.shape[1]    # empirical data covariance
+        yhat = linalg.cholesky(Cb, lower=True)
+
+        # noise_covariance = np.eye(self.noise_covariance.shape[0])  # since the data is pre whitened
+        noise_covariance = self.noise_covariance
+
+        # Choose dc
+        if self.orientation == 'fixed': dc = 1
+        elif self.orientation == 'free': dc = 3
+
+        # initializing gamma
+        wf = linalg.cholesky(self.noise_covariance, lower=True)
+        ytilde = linalg.solve(wf, yhat)
+        eta = 0.1 * (ytilde.shape[0] / np.trace (np.dot(ytilde, ytilde.T)))
+        gamma = [eta * np.eye (dc, dtype='float') for _ in range (self.sources_n)]  # Initial gamma
+        # print "Gamma = {:10f}".format(eta)
+
+        # model data covariance
+        sigma_b = noise_covariance
+        for j in range(self.sources_n):
+            sigma_b = sigma_b + np.dot(self.lead_field[:, j * dc:(j + 1) * dc],
+                                        np.dot (gamma[j], self.lead_field[:, j * dc:(j + 1) * dc].T))
+        isigma_b = linalg.inv(sigma_b)
+
+        if verbose == 1:
+            self.objective = []
+            self.objective.append(_compute_objective(Cb, isigma_b))
+
+        if verbose:
+            start = time.time()
+
+        # champagne iterations
+        for it in xrange(self.n_iter):
+            # pre-compute some useful matrices
+            lhat = np.dot(isigma_b, self.lead_field)
+
+            # compute sigma_b for the next iteration
+            sigma_b_next = np.copy(noise_covariance)
+
+            for i in xrange (self.sources_n):
+                # update Xi
+                x = np.dot(gamma[i], np.dot(yhat.T, lhat[:, i * dc:(i + 1) * dc]).T)
+
+                # update Zi
+                z = np.dot(self.lead_field[:, i * dc:(i + 1) * dc].T, lhat[:, i * dc:(i + 1) * dc])
+
+                # update Ti
+                if dc == 1:
+                    gamma[i] = sqrt(np.dot(x, x.T)) / np.real(sqrt(z))
+                else:
+                    gamma[i] = _compute_gamma_i(z, x)
+
+                # update sigma_b for next iteration
+                sigma_b_next = sigma_b_next + np.dot(self.lead_field[:, i * dc:(i + 1) * dc],
+                                                    np.dot(gamma[i], self.lead_field[:, i * dc:(i + 1) * dc].T))
+
+            # update sigma_b
+            sigma_b = sigma_b_next
+            isigma_b = linalg.inv(sigma_b)
+            if verbose == 1:
+                self.objective.append(_compute_objective(Cb, isigma_b))
+                print("Iteration : {:}, objective value : {:f}\n".format(it+1, self.objective[it]))
+
+        if verbose:
+            end = time.time()
+            print "total time elapsed : {:f}s".format(end - start)
+
+        self.Gamma = gamma
+        self.inverse_sigma_b = isigma_b
+
+        # # inverse kernel
+        # if return_inverse_kernel:
+        #     inverse_kernel = np.copy(self.lead_field.T).astype('float64')
+        #     for i in xrange(sources_n):
+        #         inverse_kernel[i * dc:(i + 1) * dc, :] \
+        #             = np.dot (gamma[i], inverse_kernel[i * dc:(i + 1) * dc, :])
+
+        # self.inverse_kernel = np.dot(inverse_kernel, isigma_b)
+
+        if verbose:
+            self.est_data_covariance = sigma_b
+            self.emp_data_covariance = Cb
+
+    def return_inverse_operator(self):
+        """
+
+        Returns inverse operator
+
+        returns:
+        -------
+        ndarray
+        array of shape (N, K)
+
+        """
+        if self.orientation == 'free': dc = 3
+        else: dc = 1
+
+        inverse_kernel = np.copy(self.lead_field.T).astype ('float64')
+        for i in xrange (self.sources_n):
+            inverse_kernel[i * dc:(i + 1) * dc, :] \
+                = np.dot (self.Gamma[i], inverse_kernel[i * dc:(i + 1) * dc, :])
+
+        inverse_kernel = np.dot (inverse_kernel, self.inverse_sigma_b)
+
+        return inverse_kernel
+
+    def apply_inverse_operator(self, meg):
+        """
+
+        parameters
+        ----------
+        meg: NDVar
+            meg data
+
+        returns:
+        -------
+        ndvar
+        source estimates
+
+        """
+        y = meg.get_data(('sensor', 'time')) # meg data
+
+        if self.orientation == 'free': dc = 3
+        else: dc = 1
+
+        inverse_kernel = np.copy(self.lead_field.T).astype ('float64')
+        for i in xrange (self.sources_n):
+            inverse_kernel[i * dc:(i + 1) * dc, :] \
+                = np.dot (self.Gamma[i], inverse_kernel[i * dc:(i + 1) * dc, :])
+
+        inverse_kernel = np.dot(inverse_kernel, self.inverse_sigma_b)
+
+        se = np.dot(inverse_kernel, y)
+        if self.orientation == 'fixed':
+            ndvar = NDVar(se, dims=(self.source, meg.time))
+        else:
+            ndvar = NDVar(se.T.reshape(-1, self.sources_n, dc), dims=(meg.time, self.source, self.space))
+
+        return ndvar
